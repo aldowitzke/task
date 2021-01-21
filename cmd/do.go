8ac67ad871b5fc384b06680b53f775c326e67d7f
aldowitzke <aldo_mwa@hotmail.com>
2021-01-21 12:10:39 -0300

    Converts args into ints

--- cmd/do.go
+++ cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +12,18 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete.",
 	Run: func(cmd *cobra.Command, args []string) {
+		var ids []int
+
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		
+		fmt.Println(ids)
 		fmt.Println("do called")
 	},
 }
