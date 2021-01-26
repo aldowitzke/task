5ca1a2f0b8da2ffa128cc57666eb7a73a9548e19
aldowitzke <aldo_mwa@hotmail.com>
2021-01-26 12:02:09 -0300

    Add do func to cmd

--- cmd/do.go
+++ cmd/do.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/aldowitzke/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -22,9 +23,25 @@ var doCmd = &cobra.Command{
 				ids = append(ids, id)
 			}
 		}
-		
-		fmt.Println(ids)
-		fmt.Println("do called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err)
+			return
+		}
+
+		for _, id := range ids {
+			if id <= 0 || id > len(ids) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed.\n", id)
+			}
+		}
 	},
 }
 
