46c01e1eb520b31b63dab0baecf5898008a0bca0
aldowitzke <aldo_mwa@hotmail.com>
2021-01-26 12:10:11 -0300

    Fix condition on delete task

--- cmd/do.go
+++ cmd/do.go
@@ -30,7 +30,7 @@ var doCmd = &cobra.Command{
 		}
 
 		for _, id := range ids {
-			if id <= 0 || id > len(ids) {
+			if id <= 0 || id > len(tasks) {
 				fmt.Println("Invalid task number:", id)
 				continue
 			}
